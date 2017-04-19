fe5ddebdb265b8c3c63c858f2ec2c0c7d9ea3394
Quinton Pike <qrpike@gmail.com>
2017-04-18 23:41:32 -0400

    better syntax

--- golang/main.go
+++ golang/main.go
@@ -7,7 +7,7 @@ import (
 
 func main(){
 
-	var servers = "nats://nats1.polygon.io:30401, nats://nats2.polygon.io:30402, nats://nats3.polygon.io:30403"
+	servers := "nats://nats1.polygon.io:30401, nats://nats2.polygon.io:30402, nats://nats3.polygon.io:30403"
 	nc, _ := nats.Connect(servers, nats.Token("YourAPIKeyHere"))
 	
 	// Subscribe to all Currency/FOREX data
