fed8850771cda9a8a2da98a7521d2c26180b6fdf
Quinton Pike <qrpike@gmail.com>
2018-09-07 01:24:50 -0400

    simplify

--- websockets/golang/main.go
+++ websockets/golang/main.go
@@ -9,34 +9,22 @@ const APIKEY = "YOUR_API_KEY"
 const CHANNELS = "C.AUD/USD,C.USD/EUR,C.USD/JPY"
 
 
-func readLoop( c *websocket.Conn ){
+func main(){
+	c, _, err := websocket.DefaultDialer.Dial("wss://socket.polygon.io/forex", nil ); if err != nil {
+		panic( err )
+	}
 	defer c.Close()
+
+	_ = c.WriteMessage( websocket.TextMessage, []byte( fmt.Sprintf("{\"action\":\"auth\",\"params\":\"%s\"}", APIKEY) ) )
+	_ = c.WriteMessage( websocket.TextMessage, []byte( fmt.Sprintf("{\"action\":\"subscribe\",\"params\":\"%s\"}", CHANNELS) ) )
+	
 	var msg interface{}
 	for {
-		err := c.ReadJSON( &msg )
-		if err != nil {
+		err := c.ReadJSON( &msg ); if err != nil {
 			panic( err )
 		}
 		fmt.Println( "Message:", msg )
 	}
+
 }
 
-
-func main(){
-	c, _, err := websocket.DefaultDialer.Dial("wss://socket.polygon.io/forex", nil )
-	if err != nil {
-		panic( err )
-	}
-	go readLoop( c )
-
-	_ = c.WriteMessage( websocket.TextMessage, []byte( fmt.Sprintf("{\"action\":\"auth\",\"params\":\"%s\"}", APIKEY) ) )
-	_ = c.WriteMessage( websocket.TextMessage, []byte( fmt.Sprintf("{\"action\":\"subscribe\",\"params\":\"%s\"}", CHANNELS) ) )
-
-	done := make(chan struct{})
-	for {
-		select {
-		case <-done:
-			return
-		}
-	}
-}
