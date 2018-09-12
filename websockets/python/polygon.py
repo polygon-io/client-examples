e205dfbae99edfbadd8e61b2a4f7a7655cf23bcf
Quinton Pike <qrpike@gmail.com>
2018-09-12 03:58:40 -0400

    Python Example

---
+++ websockets/python/polygon.py
@@ -0,0 +1,28 @@
+import websocket
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+import time
+
+def on_message(ws, message):
+    print(message)
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws):
+    print("### closed ###")
+
+def on_open(ws):
+	ws.send('{"action":"auth","params":"YOUR_API_KEY"}')
+	ws.send('{"action":"subscribe","params":"C.AUD/USD,C.USD/EUR,C.USD/JPY"}')
+
+if __name__ == "__main__":
+    # websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("wss://socket.polygon.io/forex",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
