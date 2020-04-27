2fd93f32d5292ded9f91411c18a74c390ef639d0
Quinton Pike <qrpike@gmail.com>
2020-04-27 12:38:23 -0400

    Remove NATS and update Go to be more concurrent.

--- README.md
+++ README.md
@@ -10,11 +10,6 @@ Below are some examples of how to connect and use Polygon.io. Feel free to submi
 
 ---
 
-### NATS Clients
-NATS is now deprecated and is no longer supported. 
-We encourage users to switch to using the websocket service as soon as possible. 
-It's latency is lower and has better features.
-
 ### What is Polygon.io?
 Polygon.io offers real-time, low latency streams to stock and forex markets.
 
--- websockets/golang/main.go
+++ websockets/golang/main.go
@@ -2,29 +2,45 @@ package main
 
 import (
 	"fmt"
+
 	websocket "github.com/gorilla/websocket"
+	"github.com/sirupsen/logrus"
 )
 
 const APIKEY = "YOUR_API_KEY"
-const CHANNELS = "C.AUD/USD,C.USD/EUR,C.USD/JPY"
+const CHANNELS = "T.SPY,Q.SPY"
 
-
-func main(){
-	c, _, err := websocket.DefaultDialer.Dial("wss://socket.polygon.io/forex", nil ); if err != nil {
-		panic( err )
+func main() {
+	c, _, err := websocket.DefaultDialer.Dial("wss://socket.polygon.io/stocks", nil)
+	if err != nil {
+		panic(err)
 	}
 	defer c.Close()
 
-	_ = c.WriteMessage( websocket.TextMessage, []byte( fmt.Sprintf("{\"action\":\"auth\",\"params\":\"%s\"}", APIKEY) ) )
-	_ = c.WriteMessage( websocket.TextMessage, []byte( fmt.Sprintf("{\"action\":\"subscribe\",\"params\":\"%s\"}", CHANNELS) ) )
-	
-	var msg interface{}
-	for {
-		err := c.ReadJSON( &msg ); if err != nil {
-			panic( err )
+	_ = c.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("{\"action\":\"auth\",\"params\":\"%s\"}", APIKEY)))
+	_ = c.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("{\"action\":\"subscribe\",\"params\":\"%s\"}", CHANNELS)))
+
+	// Buffered channel to account for bursts or spikes in data:
+	chanMessages := make(chan interface{}, 10000)
+
+	// Read messages off the buffered queue:
+	go func() {
+		for msgBytes := range chanMessages {
+			logrus.Info("Message Bytes: ", msgBytes)
 		}
-		fmt.Println( "Message:", msg )
+	}()
+
+	// As little logic as possible in the reader loop:
+	for {
+		var msg interface{}
+		err := c.ReadJSON(&msg)
+		// Ideally use c.ReadMessage instead of ReadJSON so you can parse the JSON data in a
+		// separate go routine. Any processing done in this loop increases the chances of disconnects
+		// due to not consuming the data fast enough.
+		if err != nil {
+			panic(err)
+		}
+		chanMessages <- msg
 	}
 
 }
-
